371a6da50466663b7fd6aae0e0a3fff78b2d6e2c
Iks Klatka <iga.klatka@gmail.com>
2023-07-05 12:47:39 +0200

    extended __init__ by Rental and Client imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
 from models.books import BookModel
 from models.countries import CountryModel
+from models.rents import BookRentModel
+from models.clients import ClientModel
--- resources/rents.py
+++ resources/rents.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import db
 from models import BookRentModel
-from schema import PlainRentSchema
+from schema import PlainRentSchema, UpdateRentSchema
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 blp = Blueprint("BookRental", __name__, description="Book rental CRUD.")
@@ -11,21 +11,54 @@ blp = Blueprint("BookRental", __name__, description="Book rental CRUD.")
 @blp.route('/rental/<string:rent_id>')
 class BookRental(MethodView):
 
-    def get(self):
-        pass
+    @blp.response(200, PlainRentSchema)
+    def get(self, rent_id):
+        book_rent = BookRentModel.query.get_or_404(rent_id)
+        return book_rent
 
-    def delete(self):
-        pass
+    def delete(self, rent_id):
+        book_rent = BookRentModel.query.get_or_404(rent_id)
+        try:
+            db.session.delete(book_rent)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, "SQLAlchemyError occurred while deleting BookRental from db.")
 
-    def put(self):
-        pass
+        return {"message": "Rent {} successfully deleted".format(rent_id)}
 
+    @blp.arguments(UpdateRentSchema)
+    @blp.response(201, PlainRentSchema)
+    def put(self, rent_data, rent_id):
+
+        book_rent = BookRentModel.query.get(rent_id)
+
+        if book_rent:
+            book_rent.date_returned = rent_data['date_returned']
+            book_rent.status = rent_data['status']
+        else:
+            book_rent = BookRentModel(id=rent_id, **rent_data)
+
+        db.session.add(book_rent)
+        db.session.commit()
+
+        return book_rent
 
 @blp.route('/rental')
 class ListBookRental(MethodView):
 
+    @blp.response(200, PlainRentSchema(many=True))
     def get(self):
-        pass
+        return BookRentModel.query.all()
 
-    def post(self):
-        pass
+    @blp.arguments(PlainRentSchema)
+    @blp.response(201, PlainRentSchema)
+    def post(self, rent_data):
+
+        book_rent = BookRentModel(**rent_data)
+        try:
+            db.session.add(book_rent)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, "SQLAlchemyError occurred while inserting rental data to db.")
+
+        return book_rent
