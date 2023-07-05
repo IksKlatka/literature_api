e258ea6a238588d4d6c8cbf2769954da24d7c2d6
Iks Klatka <iga.klatka@gmail.com>
2023-07-05 12:49:26 +0200

    Revert "extended __init__ by Rental and Client imports"
    This reverts commit 371a6da50466663b7fd6aae0e0a3fff78b2d6e2c.

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,2 @@
 from models.books import BookModel
 from models.countries import CountryModel
-from models.rents import BookRentModel
-from models.clients import ClientModel
--- models/rents.py
+++ models/rents.py
@@ -1,6 +1,6 @@
-from db import db
 import enum
-from sqlalchemy import Enum
+
+from db import db
 class RentStatus(enum.Enum): #note: for validation
     rented = "rented"
     extended = "rent extended"
@@ -9,12 +9,12 @@ class RentStatus(enum.Enum): #note: for validation
 class BookRentModel(db.Model):
     __tablename__ = "book_rent"
 
-    id = db.Column(db.Integer, primary_key=True)
-    book_id = db.Column(db.Integer, db.ForeignKey("books.id"), unique=False, nullable=False)
-    client_id = db.Column(db.Integer, db.ForeignKey("clients.id"), unique=False, nullable=False)
-    date_rented = db.Column(db.Date, unique=False, nullable=False)
-    date_returned = db.Column(db.Date, unique=False, nullable=True)
-    status = db.Column(Enum(RentStatus), nullable=False, unique=False)
+    id = db.Column(db.Int(), primary_key=True)
+    book_id = db.Column(db.Int(), db.ForeignKey("books.id"), unique=False, nullable=False)
+    client_id = db.Column(db.Int(), db.ForeignKey("clients.id"), unique=False, nullable=False)
+    date_rented = db.Column(db.Date(), unique=False, nullable=False)
+    date_returned = db.Column(db.Date(), unique=False, nullable=True)
+    status = db.Column(enum.Enum(['rented', 'extended', 'returned']), nullable=False, unique=False)
 
     book = db.relationship("BookModel", back_populates="book_rent", foreign_keys=[book_id])
-    client = db.relationship("ClientModel", back_populates="book_rent")
+    client = db.relationship("ClientModel", back_populates="book_rent", unique=False, nullable=False)
